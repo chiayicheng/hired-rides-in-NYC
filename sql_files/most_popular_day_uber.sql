c3346b3d40fd616afb1ce157548620bd39b34ce4
BoZhang1002 <116607779+BoZhang1002@users.noreply.github.com>
2022-12-06 01:26:14 -0500

    Delete sql_files directory

--- sql_files/most_popular_day_uber.sql
+++
@@ -1,6 +0,0 @@
-
-SELECT WEEK, COUNT(*) as count 
-FROM uber_trips
-WHERE pickup_datetime between '2009-01-01' and '2015-06-30'
-GROUP BY WEEK 
-ORDER BY count DESC 
--- sql_files/most_popular_hour_taxi.sql
+++
@@ -1,6 +0,0 @@
-
-SELECT HOUR, COUNT(*) as count 
-FROM taxi_trips
-WHERE tpep_pickup_datetime between '2009-01-01' and '2015-06-30'
-GROUP BY HOUR 
-ORDER BY count DESC 
